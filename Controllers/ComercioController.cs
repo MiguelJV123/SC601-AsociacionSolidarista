cbdaa6eca7ece71923cec19277c2601952a49a3c
MiguelJV123 <miguevalve@gmail.com>
2026-07-17 17:47:39 -0600

    Implementa listado de comercios con Repository Pattern

---
+++ Controllers/ComercioController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using proyecto.asociacionsolidarista.Infrastructure.DbContexts;
+using proyecto.asociacionsolidarista.Infrastructure.Repositories;
+
+namespace proyecto.asociacionsolidarista.Controllers
+{
+    public class ComercioController : Controller
+    {
+        private readonly IComercioRepository _repository;
+
+        public ComercioController()
+        {
+            var context = new AsociacionSolidaristaDbContext();
+
+            _repository = new ComercioRepository(context);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var comercios = _repository.GetAll();
+
+            return View(comercios);
+        }
+    }
+}
