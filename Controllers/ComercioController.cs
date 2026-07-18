9729f7c2f0b8ca934f054d95aa60f261f1ab4074
MiguelJV123 <miguevalve@gmail.com>
2026-07-17 18:03:22 -0600

    Implementa CRUD inicial de Comercio con Repository Pattern

--- Controllers/ComercioController.cs
+++ Controllers/ComercioController.cs
@@ -1,6 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using proyecto.asociacionsolidarista.Infrastructure.DbContexts;
 using proyecto.asociacionsolidarista.Infrastructure.Repositories;
+using proyecto.asociacionsolidarista.Models.Entities;
 
 namespace proyecto.asociacionsolidarista.Controllers
 {
@@ -11,7 +13,6 @@ namespace proyecto.asociacionsolidarista.Controllers
         public ComercioController()
         {
             var context = new AsociacionSolidaristaDbContext();
-
             _repository = new ComercioRepository(context);
         }
 
@@ -21,6 +22,100 @@ namespace proyecto.asociacionsolidarista.Controllers
             var comercios = _repository.GetAll();
 
             return View(comercios);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new Comercio
+            {
+                FechaDeRegistro = DateTime.Now,
+                Estado = 1
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Comercio comercio)
+        {
+            if (ModelState.IsValid)
+            {
+                comercio.FechaDeRegistro = DateTime.Now;
+                comercio.FechaDeModificacion = null;
+
+                _repository.Add(comercio);
+
+                TempData["Success"] = "Comercio registrado correctamente.";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(comercio);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var comercio = _repository.GetById(id);
+
+            if (comercio == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(comercio);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var comercio = _repository.GetById(id);
+
+            if (comercio == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(comercio);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Comercio comercio)
+        {
+            if (ModelState.IsValid)
+            {
+                comercio.FechaDeModificacion = DateTime.Now;
+
+                _repository.Update(comercio);
+
+                TempData["Success"] = "Comercio actualizado correctamente.";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(comercio);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var comercio = _repository.GetById(id);
+
+            if (comercio == null)
+            {
+                TempData["Error"] = "El comercio no existe.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            _repository.Remove(comercio);
+
+            TempData["Success"] = "Comercio eliminado correctamente.";
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
